75244acc1bac4bd8bbadcb6d7a8e469793ad4ab6
lucasven <lucas.venturella@hotmail.com>
2020-03-17 11:38:52 -0300

    injecao de dependencia

---
+++ DemoDI/Cases/VidaReal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoDI
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+
+    public interface IClienteRepository
+    {
+        void AdicionarCliente(Cliente cliente);
+    }
+
+    public class ClienteRepository : IClienteRepository
+    {
+        public void AdicionarCliente(Cliente cliente)
+        {
+            //vai fazer alguma coisa
+        }
+    }
+
+    public interface IClienteService
+    {
+        void AdicionarCliente(Cliente cliente);
+    }
+
+    public class ClienteService : IClienteService
+    {
+        private readonly IClienteRepository clienteRepository;
+
+        public ClienteService(IClienteRepository clienteRepository)
+        {
+            this.clienteRepository = clienteRepository;
+        }
+
+
+        public void AdicionarCliente(Cliente cliente)
+        {
+            clienteRepository.AdicionarCliente(cliente);
+        }
+    }
+}
---
+++ DemoDI/Controllers/VidaRealController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoDI.Controllers
+{
+    public class VidaRealController : Controller
+    {
+        private readonly IClienteService clienteService;
+
+        public VidaRealController(IClienteService clienteService)
+        {
+            this.clienteService = clienteService;
+        }
+
+        public void Index()
+        {
+            clienteService.AdicionarCliente(new Cliente());
+            //return View();
+        }
+    }
+}
